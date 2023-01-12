dfbf573b64f7eea6c664d1cfd9f80bc8bf2b11cd
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-12 09:59:41 +0300

    Created navbar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
+
 import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,13 +13,21 @@ export default function Home() {
         <meta name="description" content="George Kimani Porfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-
-        <section className="bg-blue-700 h-screen min-h-screen ">
-          ygbnm
-
+      <main className="bg-white px-10">
+        <section className=" min-h-screen ">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl ">George Kimani</h1>
+            <ul className="flex items-center space-x-10">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-xl" /> 
+              </li>
+              <li>
+                <a  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md"
+                href="/about">About</a>
+              </li>
+            </ul>
+          </nav>
         </section>
-      
       </main>
     </div>
   );
